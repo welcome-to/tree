d50a84378259dd7e84cd9fe7211dd771a403db73
small boo <malenkaya.bu@gmail.com>
2019-11-29 19:33:55 +0300

    doesn't work on test 9

--- binary_tree/tree.py
+++ binary_tree/tree.py
@@ -22,7 +22,33 @@ class Node(object):
                 self.rchild = child
             else:
                 self.rchild.add_child(child)
-    
+    def clear_data(self):
+        if self.lchild!=None:
+            self.lchild.clear_data()
+        if self.rchild!=None:
+            self.rchild.clear_data()
+        self.data = None
+        self.under_left = 0
+        self.under_right = 0
+
+    def add_data_to_node(self,key,data):
+        if self.key==key:
+            self.data=data
+        elif self.key>key:
+            self.under_left+=1
+            self.lchild.add_data_to_node(key,data)
+        else:
+            self.under_right+=1
+            self.rchild.add_data_to_node(key,data)
+
+    def strange_find(self,strange_key,add=0):
+        if self.under_left+add==strange_key:
+            return self.data
+        elif self.under_left+add>strange_key:
+            return self.lchild.strange_find(strange_key,add=add)
+        else:
+            return self.rchild.strange_find(strange_key,add=add+self.under_left+1)
+
     def get_data_of_node(self,key,current_l = 0):
         if key == current_l+self.under_left:
             return self.data
@@ -62,4 +88,23 @@ def load_tree(listik,tree=None,add = 0):
     if len(listik[(len(listik)//2):])>1:
         load_tree(listik[(len(listik)//2)+1:],tree=tree,add=add+len(listik)//2+1)
     return tree
-load_tree([i for i in range(100000)])
+
+
+a = int(input())
+listik = list(map(int,input().split(' ')))
+listik1 = sorted([(i,listik[i]) for i in range(len(listik))],key=lambda x: (-x[1],x[0]))
+tree = load_tree(listik)
+tree.clear_data()
+
+ask = int(input())
+unsorted_asks = list(enumerate([list(map(int,input().split(' '))) for i in range(ask)]))
+asks = sorted(unsorted_asks,key=lambda x:x[1][0])
+current_k = 0
+for i in range(len(asks)):
+    while current_k<asks[i][1][0]:
+        tree.add_data_to_node(listik1[current_k][0],listik1[current_k][1])
+        current_k+=1
+    asks[i][1].append(tree.strange_find(asks[i][1][1]-1))
+len(asks)
+for i in sorted(asks,key=lambda x:x[0]):
+    print(i[1][2])
